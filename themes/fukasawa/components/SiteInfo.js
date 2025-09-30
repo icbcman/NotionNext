fb9d75e307ecf3880ba217d59280920f4bd54a2b
icbcman <60688812+icbcman@users.noreply.github.com>
2025-09-30 16:54:25 +0800

    Update footer

--- themes/fukasawa/components/SiteInfo.js
+++ themes/fukasawa/components/SiteInfo.js
@@ -41,9 +41,9 @@ function SiteInfo({ title }) {
         <span className='text-xs font-serif'>
           Powered by
           <a
-            href='https://github.com/tangly1024/NotionNext'
+            href='https://github.com/icbcman/NotionNext'
             className='underline'>
-            NotionNext {siteConfig('VERSION')}
+            Notion-Next {siteConfig('VERSION')}
           </a>
         </span>
         <br />
