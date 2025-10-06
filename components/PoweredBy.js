28d8a182c0672a8bccd93e89424c1bd999309975
icbcman <60688812+icbcman@users.noreply.github.com>
2025-10-06 14:37:39 +0800

    Update 修改底部PoweredBy

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -9,9 +9,9 @@ export default function PoweredBy(props) {
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://github.com/icbcman/NotionNext'
+        href='https://github.com/icbcman'
         className='underline justify-start'>
-        NotionNext- {siteConfig('VERSION')}
+        All Notion Tools- {siteConfig('VERSION')}
       </a>
       .
     </div>
