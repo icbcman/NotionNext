66b0a1b350e3a31d084509566b332a9aa6540b21
icbcman <60688812+icbcman@users.noreply.github.com>
2025-10-06 15:55:17 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -9,7 +9,7 @@ export default function PoweredBy(props) {
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://github.com/icbcman'
+        href='https://notion.so/'
         className='underline justify-start'>
         All Notion Tools- {siteConfig('VERSION')}
       </a>
